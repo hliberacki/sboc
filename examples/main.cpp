8f73478572fba1a2b2508b3349b8249ec6cd3177
Hubert Liberacki <hliberacki@gmail.com>
2019-01-04 15:31:45 +0100

    Get size for all major types

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include "../include/sboc.hpp"
-
-using namespace std;
+#include <list>
+#include <map>
 
 int main()
 {
-  cout << "Hello World!" << endl;
+  std::vector<uint8_t*> v;
+  std::vector<int> val = {1,2,3,4};
+	std::list<int> vall= {1,2,3,4};
+	std::map<int, int> valm = { {1,1}, {2,2}, {3,3} };
+	std::tuple<int,int,int> valt= {3,3,3};
+  std::string s = "aaaaaa";
+  std::cout << SBOC::getSize(vall) << "\n";
+	std::cout << SBOC::getSize(val)  << "\n";
+  std::cout << SBOC::getSize(valt) << "\n";
+  //serialize(v, val);
   return 0;
 }
--- include/sboc.hpp
+++ include/sboc.hpp
@@ -1,44 +1,100 @@
 #ifndef INCLUDE_SBOC_HPP
 #define INCLUDE_SBOC_HPP
-
-#include <string>
+#include <iostream>
 #include <vector>
+#include <typeinfo>
+#include <string>
 #include <numeric>
-#include <type_traits>
 
-template<typename T>
-concept Container = requires(T con) 
+namespace SBOC
 {
-  { con.size() };  
-  { std::begin(con) };
-  { std::end(con) };
-};
+  template<typename T>
+  static size_t getSize(const T& obj);
 
-template<class T>
-concept Integral = std::is_integral_v<T>;
-
-template<typename T>
-concept Serializable = Integral<T> || Container<T> || std::is_same_v<T, std::string>;
-
-template<typename T>
-concept SerializingContainer = Container<T> && std::is_same_v<typename T::value_type, uint8_t*>;
-
-namespace sboc
-{
-	template<SerializingContainer Con, typename T>
-	void serialize(Con con, T val)
-	{
-	}
-
-	template<SerializingContainer Con, typename T>
-	T deserialize(typename Con::const_iterator& begin, typename Con::const_iterator& end)
+  template<typename T, typename ...Args>
+  static size_t getSize(const T& obj, const Args... args);
+  
+	template<typename T>
+  concept Container = requires(T con)
   {
-	}
+    { con.size() };
+    { std::begin(con) };
+    { std::end(con) };
+  };
 
-	namespace helpers
-	{
-     
-	}
+  template<class T>
+  concept Integral = std::is_integral_v<T>;
+
+  template<typename T>
+  concept Serializable = Integral<T>                    ||
+                         Container<T>                   ||
+                         std::is_same_v<T, std::string> ||
+                         std::is_floating_point_v<T>    ||
+                         std::is_array_v<T>;
+
+  template<typename T>
+  concept SerializingContainer = Container<T> &&
+                                 std::is_same_v<typename T::value_type, uint8_t*>;
+
+  template<SerializingContainer Con, Serializable S>
+  void serialize(Con con, S val)
+  {
+  }
+
+  template<SerializingContainer Con, Serializable S>
+  S deserialize(typename Con::const_iterator& begin, typename Con::const_iterator& end)
+  {
+  }
+
+  namespace helpers
+  {
+    template<Serializable T>
+    struct getSizeHelper
+    {
+      static size_t value(const T& obj) { return sizeof(T); }
+    };
+
+    template<Container C>
+    struct getSizeHelper<C>
+    {
+      static size_t value(const C& obj)
+      {
+        return std::accumulate(obj.begin(), obj.end(), sizeof(size_t),
+                               [](const size_t& sum, const typename C::value_type& current)
+                               {
+                                 return sum + helpers::getSizeHelper<typename C::value_type>::value(current);
+                               });
+      }
+    };
+
+    template <class Tuple, size_t... Is>
+    constexpr auto getSizeTupleImpl(Tuple tuple, std::index_sequence<Is...>)
+    {
+       return getSize<int>(std::get<Is>(tuple) ...);
+    }
+
+    template<class ...T>
+    struct getSizeHelper<std::tuple<T...>>
+    {
+        static size_t value(const std::tuple<T...>& obj) { return helpers::getSizeTupleImpl(obj, std::make_index_sequence<sizeof...(T)>{});}
+    };
+
+    template<>
+    struct getSizeHelper<std::string>
+    {
+      static size_t value(const std::string& obj)
+      {
+        return sizeof(size_t) + (obj.length() * sizeof(uint8_t));
+      }
+    };
+  }
+
+  template<typename T>
+  static size_t getSize(const T& obj) { return helpers::getSizeHelper<T>::value(obj); }
+
+  template<typename T, typename ...Args>
+  static size_t getSize(const T& obj, const Args... args) { return helpers::getSizeHelper<T>::value(obj) + getSize(args...); }
+
+
 }
-
 #endif //INCLUDE_SBOC_HPP
